1e84008d25a0a52b4831baca50210178a7e15bf2
ningning502 <1183189109@qq.com>
2021-09-08 16:53:12 +0800

    vue项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import xuanran from './xuanran.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+  render: h => h(xuanran),
+}).$mount('#counter')
